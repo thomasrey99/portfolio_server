2934e97068be11beb4afa3e5ec65ebfff43c0d97
thomasRey99 <thomas_rey1999@outlook.com>
2024-04-30 17:06:24 -0300

    config db deploy

--- src/database.js
+++ src/database.js
@@ -11,12 +11,18 @@ const {
     DB_HOST, 
     DB_NAME,
     DB_DIALECT,
-    DB_PORT
+    DB_PORT,
+    DB_SERVER_DEPLOY
   } = process.env;
 
-const dataBase=new Sequelize( 
+{/*const dataBase=new Sequelize( 
 `${DB_DIALECT}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
 {logging:false}
+)*/}
+
+const dataBase=new Sequelize( 
+  `${DB_SERVER_DEPLOY}`,
+  {logging:false, dialectOptions:{ssl:{require:true}}}
 )
 
 //*CONEXION DE MODELOS
